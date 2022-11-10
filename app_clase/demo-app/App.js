ac2f2e03f0bc1d9e7ede1e06f145f7f25cadba3e
Ivan Reyes <ivanrs297@gmail.com>
2022-11-10 16:29:07 -0600

    Location plus Maps

--- app_clase/demo-app/App.js
+++ app_clase/demo-app/App.js
@@ -1,94 +1,217 @@
-import React, { useState } from "react";
-import {View, Text, StyleSheet, Button, Alert, Image, SafeAreaView, TextInput} from "react-native"
-import * as ImagePicker from 'expo-image-picker'
+import React, { useEffect, useState } from "react"
+import { StyleSheet, Text, View, Button, Dimensions } from "react-native"
+import * as TaskManager from "expo-task-manager"
+import * as Location from "expo-location"
 
-const App = () => {
+import MapView, { Marker } from 'react-native-maps';
 
-  // hooks
-  const [count, setCount] = useState(0)
-  const [text, setText] = useState(null)
-  const [number, setNumber] = useState(null)
-  const [image, setImage] = useState(null)
+const LOCATION_TASK_NAME = "LOCATION_TASK_NAME"
+let foregroundSubscription = null
 
-  const selectImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    })
+// Location Tracking task in background
+TaskManager.defineTask(LOCATION_TASK_NAME, async ({ data, error }) => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  if (data) {
 
-    if (!result.cancelled)
-      setImage(result.uri)
+    const { locations } = data
+    const location = locations[0]
+    if (location) {
+      console.log("Location in background", location.coords)
+    }
+  }
+})
+
+export default function App() {
+  // Define position state: {latitude: number, longitude: number}
+
+  // Position state
+  // {latitude, longitude}
+  const [position, setPosition] = useState(null)
+
+
+  // Runs on the first render because of the empty array as second argument
+  useEffect(() => {
+    // Request permissions
+    const requestPermissions = async () => {
+      const foreground = await Location.requestForegroundPermissionsAsync()
+      if (foreground.granted) await Location.requestBackgroundPermissionsAsync()
+    }
+
+    requestPermissions()
+  }, [])
+
+  // Start location tracking in foreground
+  const startForegroundUpdate = async () => {
+
+    // Check if foreground permission is granted
+    const { granted } = await Location.getForegroundPermissionsAsync()
+    if (!granted) {
+      console.log("location tracking denied")
+      return
+    }
+
+    // Make sure that foreground location tracking is not running
+    // optional chaining operator ?.
+    foregroundSubscription?.remove()
+
+    // Start watching position in real-time
+    foregroundSubscription = await Location.watchPositionAsync(
+      {
+        // For better logs, we set the accuracy to the most sensitive option
+        accuracy: Location.Accuracy.BestForNavigation,
+      },
+      location => {
+        setPosition(location.coords)
+      }
+    )
   }
 
-  const createAlert = () =>
-    Alert.alert(
-      `Hello ${text}`,
-      `Your number ${number}`,
-      [
-        {
-          text: 'Cancel',
-          onPress: () => console.log("Canceled"),
-          style: "cancel"
-        },
-        {
-          text: "Ok",
-          onPress: () => console.log("Ok pressed")
-        }
-      ]
+  // Stop location tracking in foreground
+  const stopForegroundUpdate = () => {
+    foregroundSubscription?.remove()
+    setPosition(null)
+  }
+
+  // Start location tracking in background
+  const startBackgroundUpdate = async () => {
+    // Don't track position if permission is not granted
+    const { granted } = await Location.getBackgroundPermissionsAsync()
+    if (!granted) {
+      console.log("location tracking denied")
+      return
+    }
+
+    // Make sure the task is defined otherwise do not start tracking
+    const isTaskDefined = await TaskManager.isTaskDefined(LOCATION_TASK_NAME)
+    if (!isTaskDefined) {
+      console.log("Task is not defined")
+      return
+    }
+
+    // Don't track if it is already running in background
+    const hasStarted = await Location.hasStartedLocationUpdatesAsync(
+      LOCATION_TASK_NAME
     )
+    if (hasStarted) {
+      console.log("Already started")
+      return
+    }
+
+    await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+      // For better logs, we set the accuracy to the most sensitive option
+      accuracy: Location.Accuracy.BestForNavigation,
+      // Make sure to enable this notification if you want to consistently track in the background
+      showsBackgroundLocationIndicator: true,
+      foregroundService: {
+        notificationTitle: "Location",
+        notificationBody: "Location tracking in background",
+        notificationColor: "#fff",
+      },
+    })
+  }
+
+  // Stop location tracking in background
+  const stopBackgroundUpdate = async () => {
+    const hasStarted = await Location.hasStartedLocationUpdatesAsync(
+      LOCATION_TASK_NAME
+    )
+    if (hasStarted) {
+      await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME)
+      console.log("Location tacking stopped")
+    }
+  }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}> Counter: {count}</Text>
-      <Button
-        onPress={() => setCount(count + 1)}
-        title = "Increase"
-        color = "#FF0000"
-      />
+    <View style={styles.container}>
+      <Text>Longitude: {position?.longitude}</Text>
+      <Text>Latitude: {position?.latitude}</Text>
 
-      <TextInput
-        style={styles.input}
-        onChangeText={setText}
-        value = {text}
-        placeholder = "A string"
-        keyboardType="default"
-      />
-
-      <TextInput
-        style={styles.input}
-        onChangeText={setNumber}
-        value = {number}
-        placeholder = "A number"
-        keyboardType="numeric"
-      />
-
-      <Button 
-        title="Select Image"
-        onPress={selectImage}
-      />
-
-      <View style={styles.imageContainer}>
-        {image && <Image source={{uri: image}} style={styles.image} />}
+      <View style={{flexDirection: "row", marginVertical: 10}}>
+        <Button
+          onPress={startForegroundUpdate}
+          title="Start in foreground"
+          color="green"
+        />
+        <Button
+          onPress={stopForegroundUpdate}
+          title="Stop in foreground"
+          color = "red"
+        />
       </View>
+      
+      <View style={styles.separator} />
+      <View style={{flexDirection: "row", marginVertical: 10}}>
+        <Button
+          onPress={startBackgroundUpdate}
+          title="Start in background"
+          color="green"
+        />
+        <View style={styles.separator} />
+        <Button
+          onPress={stopBackgroundUpdate}
+          title="Stop in foreground"
+          color="red"
+        />
+        </View> 
 
-      <Button
-        title = "Submit"
-        onPress={() => createAlert()}
-        color="#16D339"
-      />
+      <MapView
+          style={styles.map} 
+          region={
+            {
+              latitude: position ? position.latitude : 20.680537208670096, 
+              longitude: position ? position.longitude : -103.34742944564384,
+              latitudeDelta: 0.04,
+              longitudeDelta: 0.05,
+            }
+           }
+          initialRegion={{
+              latitude: position ? position.latitude : 20.680537208670096, 
+              longitude: position ? position.longitude : -103.34742944564384,
+              latitudeDelta: 0.04,
+              longitudeDelta: 0.05,
+          }}
+        >
 
-    </SafeAreaView>
+          <Marker
+            coordinate={
+              {
+                latitude: position ? position.latitude : 20.680537208670096, 
+                longitude: position ? position.longitude : -103.34742944564384,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }
+            }
+            title="Example Marker"
+            description="Example description"
+          />
+        </MapView>
+
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {backgroundColor: "#EFEFEF", flex: 1, justifyContent: "center", padding: 30},
-  title: {fontSize: 50, fontWeight: "bold"},
-  image: {width: 200, height: 200, borderRadius: 100},
-  input: {height: 40, margin: 15, borderWidth: 1, padding: 10, bordeRadius: 20},
-  imageContainer: {margin: 20, alignItems: "center"}
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  button: {
+    marginTop: 15,
+  },
+  separator: {
+    marginVertical: 8,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: 300,
+  },
 })
-
-export default App;
